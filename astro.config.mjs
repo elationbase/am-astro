f2a74e1d6f3ea6766517dee8b07ebe39c21df4b7
Alvaro Montero <elationbase@gmail.com>
2026-05-16 11:22:55 -0400

    build: add wrangler for cloudflare deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: 'static',
+  adapter: cloudflare(),
 });
