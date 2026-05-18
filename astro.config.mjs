3ec586f7df77257544787ee14000996402b23cf1
Alvaro Montero <elationbase@gmail.com>
2026-05-18 19:10:07 -0400

    build: add @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://moterux.com',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
